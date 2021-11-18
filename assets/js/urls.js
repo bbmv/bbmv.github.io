8ace9f22f477881ecd96d6176cbd06872353e34a
bbmv <boboboris@gmail.com>
2021-11-19 00:27:03 +0200

    web

---
+++ assets/js/urls.js
@@ -0,0 +1,6 @@
+//export const path = 'assets/_data/test-with-pm-and-PM2.json';
+//export const path = 'assets/_data/test-request.json';
+//export const path = 'assets/_data/test2.json';
+export var path = 'https://api.inhaabit.com/v2/configurator/2d/5ae283bc9278a33eba8c75a2';
+//export const path = '/assets/_data/plinth.json'; // 'https://api.inhaabit.com/v2/configurator/2d/5ae283bc9278a33eba8c75a2';
+//# sourceMappingURL=urls.js.map
